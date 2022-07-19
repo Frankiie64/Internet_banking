144ee197f88bf3a07c346f269ba434ec3e6c4fd8
Franklyn Brea <franklynbrea100@gmail.com>
2022-07-19 12:23:44 -0400

    Funcionalidad de Pago prestamo

---
+++ Internet_banking.Core.Application/ViewModels/Clients/Paids/DeubsPaids.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Internet_banking.Core.Application.ViewModels.Clients.Paids
+{
+    public class DeubsPaids
+    {
+        [Required(ErrorMessage = "Este campo es obligatorio")]
+        [Range(1, int.MaxValue, ErrorMessage = "Debes ingresar uno de tus prestamos.")]
+        public int IdDeubs { get; set; }
+        [Required(ErrorMessage = "Este campo es obligatorio")]
+        [Range(1, int.MaxValue, ErrorMessage = "Debes ingresar una de tus cuentas de ahorro.")]
+        public int IdSaveAccount { get; set; }
+        [Required(ErrorMessage = "Este campo es obligatorio")]
+        [Range(0, double.MaxValue, ErrorMessage = "Debes ingresar un monto apartir de 0. ")]
+        public double Amount { get; set; }
+        public bool HasError { get; set; }
+        public string Error { get; set; }
+    }
+}
